5fbf5a59b6a5d82e41dd8be9eea795afb5b951df
ethan <dev.eunji.lee@gmail.com>
2022-05-24 23:36:59 +0900

    ADD: 참고 링크 주석 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, {Request, Response} from 'express'
 
+/**
+ * * 셋팅 참조 링크
+ * * https://velog.io/@qhgus/Node-Express-TypeScript-%ED%99%98%EA%B2%BD-%EC%84%B8%ED%8C%85
+ */
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {res.send('Hello World!')})
