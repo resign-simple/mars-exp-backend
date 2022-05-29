3611470bcdbdfe2dafcab373c37e3264db0f8403
ethan <dev.eunji.lee@gmail.com>
2022-05-29 23:15:45 +0900

    FIX: 포트 번호 상수(const)로 빼기

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
  * * 셋팅 참조 링크
  * * https://velog.io/@qhgus/Node-Express-TypeScript-%ED%99%98%EA%B2%BD-%EC%84%B8%ED%8C%85
  */
+
+const PORT_NUMBER = 8000;
+
 const app = express();
 
 // * ====== RestAPI ====== * //
@@ -45,13 +48,17 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(), // playground(https://www.apollographql.com/docs/apollo-server/migration/)
+    ApolloServerPluginLandingPageGraphQLPlayground(), // playground 셋팅 방법: https://www.apollographql.com/docs/apollo-server/migration/
   ],
 });
 server.start().then(() => {
-  server.applyMiddleware({ app }); // 아폴로 서버에 express와 같이 동작한다고 알려줘야 한다
-
-  app.listen({ port: 8080 }, () =>
-    console.log(`Server ready at http://localhost:${8080}${server.graphqlPath}`)
+  // 아폴로 서버에 express와 같이 동작한다고 알려줘야 한다
+  // (https://velog.io/@bard/5.-apollo-server-express-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-express-%EC%84%9C%EB%B2%84%EB%A1%9C-%EC%A0%84%ED%99%98%ED%95%98%EA%B8%B0)
+  // 공식 문서: https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+  server.applyMiddleware({ app });
+  app.listen({ port: PORT_NUMBER }, () =>
+    console.log(
+      `Server ready at http://localhost:${PORT_NUMBER}${server.graphqlPath}`
+    )
   );
 });
